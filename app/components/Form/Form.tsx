6aea3febd38b18460093680988d8d45ecedb3c8e
igor-ht <i_ht93@hotmail.com>
2024-02-14 21:47:54 +0200

    drag and drop file implemented

--- app/components/Form/Form.tsx
+++ app/components/Form/Form.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Section from './Section';
-import { useContext } from 'react';
+import { DragEvent, useContext } from 'react';
 import { FormContext } from '../ContextProvider';
 
 export default function Form() {
@@ -11,17 +11,27 @@ export default function Form() {
 		console.log(data);
 	};
 
+	const fileOnDrop = (e: DragEvent<HTMLInputElement>) => {
+		e.preventDefault();
+		// if file is not an image do nothing
+		if (!e.dataTransfer.files[0].type.startsWith('image')) return;
+		e.currentTarget.files = e.dataTransfer.files;
+		e.currentTarget.dispatchEvent(new Event('change', { bubbles: true }));
+	};
+
 	return (
 		<form
 			onSubmit={handleSubmit(onSubmit)}
 			className="w-full sm:w-2/4 h-1/2 sm:h-96 flex flex-col gap-2 sm:gap-4 p-4 border-2 border-zinc-400 rounded">
 			<Section
 				label={{ children: 'Name', rest: { htmlFor: 'name' } }}
-				input={{ rest: { type: 'text', id: 'name', name: 'name', autoComplete: 'name', required: true } }}
+				input={{ rest: { type: 'text', id: 'name', name: 'name', autoComplete: 'name', required: true, placeholder: 'Your name here' } }}
 			/>
 			<Section
 				label={{ children: 'Email', rest: { htmlFor: 'email' } }}
-				input={{ rest: { type: 'email', id: 'email', name: 'email', autoComplete: 'email', required: true } }}
+				input={{
+					rest: { type: 'email', id: 'email', name: 'email', autoComplete: 'email', required: true, placeholder: 'email@example.com' },
+				}}
 			/>
 			<Section
 				label={{ children: 'Profile Image', rest: { htmlFor: 'profileImage' } }}
@@ -32,6 +42,8 @@ export default function Form() {
 						name: 'profileImage',
 						required: true,
 						accept: '.png,.jpg,.jpeg,.jfif,.pjpeg,.pjp,.svg,.webp,.gif,.ico',
+						title: 'Drag and drop your image here or click to select a file.',
+						onDrop: fileOnDrop,
 					},
 				}}
 			/>
--- app/components/User/User.tsx
+++ app/components/User/User.tsx
@@ -5,15 +5,19 @@ import { FormContext } from '../ContextProvider';
 import { useContext } from 'react';
 
 export default function User() {
-	const { dirtyFields, watch } = useContext(FormContext);
+	const { watch } = useContext(FormContext);
+
+	const currentEmail = watch('email');
+	const currentName = watch('name');
+	const currentImageProfile = watch('profileImage')[0];
 
 	return (
 		<div className="w-full sm:w-2/4 h-1/2 sm:h-96 flex flex-col gap-2 sm:gap-4 p-4 border-2 border-zinc-400 rounded justify-evenly items-start">
 			<div className="w-40 h-40 self-center flex items-center justify-center border-2 border-stone-400 rounded-full bg-white overflow-hidden">
-				{dirtyFields?.profileImage && (
+				{currentImageProfile && (
 					<Image
 						className="object-cover w-full h-full"
-						src={URL.createObjectURL(watch('profileImage')[0])}
+						src={URL.createObjectURL(currentImageProfile)}
 						alt="profile image"
 						width={200}
 						height={200}
@@ -24,11 +28,11 @@ export default function User() {
 			<div className="flex flex-col gap-4 mt-4">
 				<span className="flex flex-row gap-2 items-center">
 					<b className="w-16 font-sans font-medium text-xl ml-1">Name:</b>
-					<p className="font-sans text-lg indent-1">{watch('name')}</p>
+					<p className="font-sans text-lg indent-1">{currentName}</p>
 				</span>
 				<span className="flex flex-row gap-2 items-center">
 					<p className="w-16 font-sans font-medium text-xl ml-1">Email:</p>
-					<p className="font-sans text-lg indent-1">{watch('email')}</p>
+					<p className="font-sans text-lg indent-1">{currentEmail}</p>
 				</span>
 			</div>
 		</div>
